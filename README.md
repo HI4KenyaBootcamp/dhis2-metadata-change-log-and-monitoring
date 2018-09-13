96a2713cba0c19a8a4b29495d9ea9b65b60e7068
Marekia Kiringu <marekiakiringu@users.noreply.github.com>
2018-09-13 12:42:49 +0300

    Rework of Metadata Audit app

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# DHIS2 Metadata Change Log and Monitoring
-The following is an installable DHIS2 application that can be accessed from within the platform. The application allows for logging and monitoring of any metadata change. The application is built with ReactJS, a JavaScript framework.
+# Metadata Audit
+DHIS2 Metadata Change Log and Monitoring app
+
+# Introduction
+Metadata Audit is an installable DHIS2 application that allows users to track the history of metadata objects on the system. The information made available via this application is made possible through accessing the `metadataAudit` Psql table.
+
+# Dependencies
+The following application requires, at the least, DHIS2 2.30.
+
+Development was done on DHIS2 2.30, therefore, we recommend that your instance of DHIS2 be upgraded this version.
+
+For this application to work, you MUST enable the metadata audit feature on your application instance. Refer [here](https://docs.dhis2.org/2.30/en/developer/html/webapi_metadata_audit.html) for instructions.
--- src/index.js
+++ src/index.js
@@ -1,8 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import TodoList from './TodoList';
+import { init, getInstance } from 'd2/lib/d2';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// initialize d2 library - local
+init({ baseUrl: 'https://test.hiskenya.org/kenya/api' });
+
+// print the currentUser name in the console
+getInstance().then(d2 => {
+  console.log(d2.currentUser.name);
+});
+
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      text: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.text.length) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ''
+    }));
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h3>Todo</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="new-todo">
+            What needs to be done?
+          </label>
+          <input
+            id="new-todo"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <button>
+            Add #{this.state.items.length + 1}
+          </button>
+        </form>
+        <TodoList items={this.state.items} />
+      </React.Fragment>
+    );
+  }
+}
+
+ReactDOM.render(<TodoApp />, document.getElementById('root'));
