f4ac115368f7a2ff0d9ead681bd95f2cebf0bf80
george-31 <mwaurag96@gmail.com>
2018-09-14 16:39:15 +0300

    fetching data elements waiting for metadata audits

--- src/Home/HomePopular.js
+++ src/Home/HomePopular.js
@@ -16,6 +16,28 @@
  */
 
 import React from 'react';
+import { init, getInstance } from 'd2/lib/d2';
+
+// initialize d2 library - local
+init({ baseUrl: 'https://test.hiskenya.org/kenya/api' });
+
+// get an instance of d2
+getInstance().then(d2 => {
+  // returns the api object
+  const api = d2.Api.getApi();
+
+  // do a get request for /api/resources
+  api.get('dataElements.json', {'fields': ':all', 'pageSize': '5'})
+  .then(resources => {
+    // for each object
+    resources.dataElements.forEach(consolePrint);
+  });
+});
+
+function consolePrint(item, index) {
+  console.log(item.id + ' | ' + item.displayName + ' | ' + item.valueType);
+}
+
 
 /* some code goes here */
 
